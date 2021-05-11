9d048718ca67b576223b22757dbba28f4a6823c3
LittleBigPlaye <robin.beck.we@gmail.com>
2021-05-11 11:00:32 +0200

    Initialized Spring Application

---
+++ src/main/java/de/fourzerofournotfound/rateyourstuff/RaYS/RaYsApplication.java
@@ -0,0 +1,13 @@
+package de.fourzerofournotfound.rateyourstuff.RaYS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaYsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaYsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/fourzerofournotfound/rateyourstuff/RaYS/RaYsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fourzerofournotfound.rateyourstuff.RaYS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaYsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
