3c5b9b539e5750ab8ed43f99a0c545ffebdd5503
Chris <christoph.frischmuth@fh-erfurt.de>
2021-05-12 12:46:30 +0200

    Example added to UserRepo

--- src/main/java/de/fourzerofournotfound/rateyourstuff/rays/repositories/UserRepository.java
+++ src/main/java/de/fourzerofournotfound/rateyourstuff/rays/repositories/UserRepository.java
@@ -4,7 +4,11 @@ import de.fourzerofournotfound.rateyourstuff.rays.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    public Optional<User> findByLastName(String lastName);
 
 }
