f42baadf3e592495ab52b96a1f727ddb4da091bd
MKnop2 <kickmepony@gmail.com>
2021-05-17 20:07:17 +0200

    Book- GameRepository added

---
+++ src/main/java/de/fourzerofournotfound/rateyourstuff/rays/repositories/GameRepository.java
@@ -0,0 +1,17 @@
+package de.fourzerofournotfound.rateyourstuff.rays.repositories;
+
+import de.fourzerofournotfound.rateyourstuff.rays.models.Season;
+import de.fourzerofournotfound.rateyourstuff.rays.models.Series;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface GameRepository extends JpaRepository<Series, Long> {
+
+}
+
