6ffd2cb13c2d250515f3179156da45384d06ff6d
Chris <christoph.frischmuth@fh-erfurt.de>
2021-05-19 15:13:59 +0200

    - LoginRoleRepositoryTest UC

--- src/test/java/de/fourzerofournotfound/rateyourstuff/rays/repositories/LoginRoleRepositoryTest.java
+++ src/test/java/de/fourzerofournotfound/rateyourstuff/rays/repositories/LoginRoleRepositoryTest.java
@@ -1,7 +1,49 @@
 package de.fourzerofournotfound.rateyourstuff.rays.repositories;
 
+import com.mysql.cj.log.Log;
+import de.fourzerofournotfound.rateyourstuff.rays.models.LoginRole;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Repository;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class LoginRoleRepositoryTest {
+
+    @Autowired
+    LoginRoleRepository loginRoleRepository;
+
+    @AfterEach
+    public void AfterEach(){loginRoleRepository.deleteAll();};
+
+    @Test
+    public void should_add_LoginRole()
+    {
+        //Given
+        LoginRole given = LoginRole.builder()
+                .build();
+
+        //When
+        LoginRole result = loginRoleRepository.save(given);
+
+        //Then
+        Assertions.assertThat(result.getLoginRoleId()).isNotNull();
+        Assertions.assertThat(result.getLoginRoleId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void should_delete_LoginRole()
+    {
+        //Given
+        LoginRole given = LoginRole.builder()
+                .build();
+        LoginRole saved = loginRoleRepository.save(given);
+
+        //When
+    }
 
 }
