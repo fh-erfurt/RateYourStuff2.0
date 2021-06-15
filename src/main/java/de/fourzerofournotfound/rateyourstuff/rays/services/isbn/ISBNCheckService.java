766018ed0da53129b18bdd0a809f8b9fd2ef97fa
Robin <robin.beck.we@gmail.com>
2021-06-15 15:23:13 +0200

    #21 added comment to isc

--- src/main/java/de/fourzerofournotfound/rateyourstuff/rays/services/isbn/ISBNCheckService.java
+++ src/main/java/de/fourzerofournotfound/rateyourstuff/rays/services/isbn/ISBNCheckService.java
@@ -1,10 +1,17 @@
 package de.fourzerofournotfound.rateyourstuff.rays.services.isbn;
 
 import de.fourzerofournotfound.rateyourstuff.rays.models.Book;
+import de.fourzerofournotfound.rateyourstuff.rays.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("ics")
 public class ISBNCheckService {
+    /**
+     * Checks if the given book has either a valid ISBN10 or ISBN13
+     * @param book the book that should be checked
+     * @return true, if the ISBN is valid, false, if the isbn is invalid
+     */
     public boolean checkIfISBNisValid(Book book) {
         return (ISBN13.isValid(book.getIsbn()) || ISBN10.isValid(book.getIsbn()));
     }
