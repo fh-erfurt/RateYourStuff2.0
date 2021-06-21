c92e05354feb340e11deb9482c7715cbdd53e98f
Avartos <59014772+Avartos@users.noreply.github.com>
2021-06-21 22:26:43 +0200

    losing my religion. ca 100 testdaten hinzugefügt. datenbanke neu machen und dann das sql-skript importieren. Manche tabs sind nicht befüllt

--- src/main/java/de/fourzerofournotfound/rateyourstuff/rays/repositories/PersonAssigmentRepository.java
+++ src/main/java/de/fourzerofournotfound/rateyourstuff/rays/repositories/PersonAssigmentRepository.java
@@ -7,5 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface PersonAssigmentRepository extends JpaRepository<PersonAssignment,Long> {
-    Optional<PersonAssignment> findById (Long id);
 }
