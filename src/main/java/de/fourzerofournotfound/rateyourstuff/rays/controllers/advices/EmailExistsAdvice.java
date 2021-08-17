52a7ed4173fac4e1e1e6daa2cee0ce9a7a81d8ef
Chris <christoph.frischmuth@fh-erfurt.de>
2021-08-17 10:47:45 +0200

    # EmailExistsAdvice // fixed

--- src/main/java/de/fourzerofournotfound/rateyourstuff/rays/controllers/advices/EmailExistsAdvice.java
+++ src/main/java/de/fourzerofournotfound/rateyourstuff/rays/controllers/advices/EmailExistsAdvice.java
@@ -2,13 +2,15 @@ package de.fourzerofournotfound.rateyourstuff.rays.controllers.advices;
 
 import de.fourzerofournotfound.rateyourstuff.rays.services.errors.EmailAlreadyExistsException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ControllerAdvice
 public class EmailExistsAdvice {
     @ResponseBody
     @ExceptionHandler(EmailAlreadyExistsException.class)
     @ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
-    String seriesNotFoundHandler(EmailAlreadyExistsException existsException) {return existsException.getMessage();}
+    String emailExistsAdvice(EmailAlreadyExistsException existsException) {return existsException.getMessage();}
 }
