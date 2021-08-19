322a40ddaa68f69d636c96df677ce2ecc82d4a28
Robin <robin.beck.we@gmail.com>
2021-08-19 11:33:37 +0200

    added javadoc to RatingController

--- src/main/java/de/fourzerofournotfound/rateyourstuff/rays/controllers/RatingController.java
+++ src/main/java/de/fourzerofournotfound/rateyourstuff/rays/controllers/RatingController.java
@@ -1,12 +1,12 @@
 package de.fourzerofournotfound.rateyourstuff.rays.controllers;
 
 import de.fourzerofournotfound.rateyourstuff.rays.dtos.RatingDto;
-import de.fourzerofournotfound.rateyourstuff.rays.models.Rating;
+import de.fourzerofournotfound.rateyourstuff.rays.errors.InvalidRatingException;
 import de.fourzerofournotfound.rateyourstuff.rays.errors.RatingNotFoundException;
+import de.fourzerofournotfound.rateyourstuff.rays.models.Rating;
 import de.fourzerofournotfound.rateyourstuff.rays.repositories.RatingRepository;
 import de.fourzerofournotfound.rateyourstuff.rays.services.PageableService;
 import de.fourzerofournotfound.rateyourstuff.rays.services.RatingService;
-import de.fourzerofournotfound.rateyourstuff.rays.errors.InvalidRatingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -19,6 +19,14 @@ import java.util.Optional;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
+/**
+ * <p>This Controller provides basic REST Interfaces to interact with Rating entities from the database</p>
+ *
+ * @author Christoph Frischmuth
+ * @author John Klippstein
+ * @author Mickey Knop
+ * @author Robin Beck
+ */
 @RestController
 @RequestMapping("/rest/ratings")
 public class RatingController {
@@ -35,6 +43,15 @@ public class RatingController {
         this.ratingService = ratingService;
     }
 
+    /**
+     * This method is used to get all ratings from the database
+     *
+     * @param page    the current page (optional)
+     * @param size    the number of items per page
+     * @param orderBy the attributed that should be ordered
+     * @param order   the order (asc, desc)
+     * @return a list of rating dtos
+     */
     @GetMapping("/all")
     ResponseEntity<List<RatingDto>> getAll(
             @RequestParam(defaultValue = "0") int page,
@@ -51,6 +68,16 @@ public class RatingController {
         );
     }
 
+    /**
+     * This method is used to get all ratings for a specific medium from the database
+     *
+     * @param page     the current page (optional)
+     * @param size     the number of items per page
+     * @param orderBy  the attributed that should be ordered
+     * @param order    the order (asc, desc)
+     * @param mediumId the id of the medium that should be searched
+     * @return a list of rating dtos
+     */
     @GetMapping("/all/medium/{mediumId}")
     ResponseEntity<List<RatingDto>> getAllByMediumId(
             @RequestParam(defaultValue = "0") int page,
@@ -68,6 +95,16 @@ public class RatingController {
         );
     }
 
+    /**
+     * This method is used to get all ratings with a specific user id from the database
+     *
+     * @param page    the current page (optional)
+     * @param size    the number of items per page
+     * @param orderBy the attributed that should be ordered
+     * @param order   the order (asc, desc)
+     * @param userId  the id of the user that should be searched
+     * @return a list of rating dtos
+     */
     @GetMapping("/all/user/{userId}")
     ResponseEntity<List<RatingDto>> getAllByUserId(
             @RequestParam(defaultValue = "0") int page,
@@ -85,6 +122,13 @@ public class RatingController {
         );
     }
 
+    /**
+     * This method is used to get a specific rating by its id from the database
+     *
+     * @param id the id of the rating
+     * @return a dto of the found rating
+     * @throws RatingNotFoundException if there is no rating with the given id
+     */
     @GetMapping("/{id}")
     ResponseEntity<RatingDto> getById(@PathVariable Long id) throws RatingNotFoundException {
         Optional<Rating> rating = ratingRepository.findById(id);
@@ -95,22 +139,39 @@ public class RatingController {
         }
     }
 
+    /**
+     * This method is used to get a rating from a specific user for a specific medium from the database
+     *
+     * @param userId   the id of the user that is linked to the rating
+     * @param mediumId the id of the medium that is linked to the rating
+     * @return a dto of the found rating
+     * @throws RatingNotFoundException if there is no rating for the given user and medium
+     */
     @GetMapping("/user/{userId}/medium/{mediumId}")
     ResponseEntity<RatingDto> getByUserIdAndByMediumId(@PathVariable Long userId, @PathVariable Long mediumId) throws RatingNotFoundException {
         Optional<Rating> rating = ratingRepository.findByMediumIdAndUserId(mediumId, userId);
-        if(rating.isPresent()) {
+        if (rating.isPresent()) {
             return ResponseEntity.ok(ratingService.convertToDto(rating.get()));
         }
         throw new RatingNotFoundException("There is no " + Rating.class.getSimpleName() + " for userId " + userId + " and mediumId " + mediumId);
     }
 
 
+    /**
+     * This method is used to add a new rating to the database.
+     * In order to prevent duplicate ratings in the database, this method automatically checks if there is already a rating with the same user id and medium id.
+     * If there is already an existing rating, it will be updated
+     *
+     * @param rating the rating that should be added to the database
+     * @return a dto of the newly added (or updated, if it already existed) rating
+     * @throws InvalidRatingException if the rating contains invalid values
+     */
     @PreAuthorize("hasAuthority('User')")
     @PostMapping(path = "/add", consumes = "application/json", produces = "application/json")
     ResponseEntity<RatingDto> add(@RequestBody Rating rating) throws InvalidRatingException {
         Optional<Rating> foundRating = ratingRepository.findByMediumIdAndUserId(rating.getMediumMappingId(), rating.getUserMappingId());
         //check if there is already a rating from the same user for the same medium
-        if(foundRating.isPresent()) {
+        if (foundRating.isPresent()) {
             //set the id of the incoming rating to make sure that is gets overwritten
             rating.setId(foundRating.get().getId());
         }
@@ -122,6 +183,13 @@ public class RatingController {
     }
 
 
+    /**
+     * This method is used to update an existing rating. It updates the description and the given points of the rating
+     *
+     * @param rating the rating that should be updated
+     * @return a dto of the saved rating
+     * @throws RatingNotFoundException if there is no rating with the given rating id
+     */
     @PreAuthorize("hasAuthority('User')")
     @PutMapping(consumes = "application/json", produces = "application/json")
     ResponseEntity<RatingDto> update(@RequestBody Rating rating) throws RatingNotFoundException {
@@ -138,11 +206,18 @@ public class RatingController {
         throw new RatingNotFoundException("No " + Rating.class.getSimpleName() + " found with id " + rating.getId());
     }
 
+    /**
+     * Deletes the given rating
+     *
+     * @param id the id of the rating that should be deleted
+     * @return http status 200
+     * @throws RatingNotFoundException if there is no rating with the given id
+     */
     @PreAuthorize("hasAuthority('User')")
     @DeleteMapping("/{id}")
     HttpStatus deleteRating(@PathVariable Long id) throws RatingNotFoundException {
         Optional<Rating> rating = ratingRepository.findById(id);
-        if(rating.isPresent()) {
+        if (rating.isPresent()) {
             this.ratingRepository.deleteById(id);
             logger.info("Removed " + Rating.class.getSimpleName() + " with id " + id);
             return HttpStatus.OK;
@@ -150,6 +225,12 @@ public class RatingController {
         throw new RatingNotFoundException("There is no " + Rating.class.getSimpleName() + " with id " + id);
     }
 
+    /**
+     * This Method is used to count the number of ratings for a certain medium
+     *
+     * @param mediumId the id of the medium for which ratings should be counted
+     * @return the number of ratings for the given medium
+     */
     @GetMapping("/count/{mediumId}")
     ResponseEntity<Long> countRatings(@PathVariable Long mediumId) {
         return ResponseEntity.ok(this.ratingRepository.countAllByMediumId(mediumId));
